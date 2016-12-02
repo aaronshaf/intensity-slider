f9b2b8149df2c3d6ee0ed0eb5c6ee6dc2b1c7c82
Aaron Shafovaloff <aaronshaf@gmail.com>
2016-12-01 19:59:27 -0700

    fixes

--- components/index.js
+++ components/index.js
@@ -5,12 +5,12 @@ import preact, { Component } from 'preact'
 export default class MyComponent extends Component {
   constructor (props) {
     super(props)
-    this.props.input.addEventListener('focus', this.handleFocus)
-    this.props.input.addEventListener('blur', this.handleBlur)
-    this.props.input.addEventListener('input', this.handleInput)
+    props.input.addEventListener('focus', this.handleFocus)
+    props.input.addEventListener('blur', this.handleBlur)
+    props.input.addEventListener('input', this.handleInput)
     this.state = {
       hasFocus: false,
-      intensity: this.props.input.value,
+      intensity: props.input.value,
       isSliding: false
     }
   }
@@ -20,6 +20,7 @@ export default class MyComponent extends Component {
   handleInput = () => { this.setState({ intensity: this.props.input.value }) }
   handleMouseDown = (event) => {
     this.setState({ isSliding: true })
+    this.handleSlide(event)
     document.addEventListener('mouseup', this.handleMouseUp)
   }
   handleMouseMove = (event) => {
@@ -32,9 +33,13 @@ export default class MyComponent extends Component {
     document.removeEventListener('mouseup', this.handleMouseUp)
   }
   handleSlide = (event) => {
-    const intensity = Math.round(
-      ((event.clientX - event.target.offsetLeft) / event.target.width) * 10
+    const max = parseInt(this.props.input.max, 10)
+    let intensity = Math.round(
+      ((event.clientX - this.img.offsetLeft) / this.img.width) * max
     ) - 1
+    if (intensity > max) {
+      intensity = max
+    }
     this.setState({ intensity })
     this.props.input.value = intensity
     this.props.input.focus()
@@ -54,9 +59,9 @@ export default class MyComponent extends Component {
 
   render () {
     return (
-      <div style='position: relative; height: 300px;'>
+      <div style='position: relative; height: 300px;width: 300px;user-select: none;'>
         <img
-          style={`width: 250px; height: 250px;border: 10px solid ${this.state.hasFocus ? 'blue' : 'transparent'}`}
+          style={`user-select: none;width: 250px; height: 250px;border: 10px solid ${this.state.hasFocus ? 'blue' : 'transparent'}`}
           onClick={this.handleClick}
           onMouseDown={this.handleMouseDown}
           onMouseMove={this.handleMouseMove}
@@ -65,7 +70,12 @@ export default class MyComponent extends Component {
           ref={this.setImg}
           src="https://d3vv6lp55qjaqc.cloudfront.net/items/0s2x113v3p3R0i0D1G00/Screen%20Shot%202016-12-01%20at%204.22.24%20PM.png?X-CloudApp-Visitor-Id=8c7c3ddb4f82754e00f6dac0eaa0cbfa&v=416b811a"
         />
-        <div style={`position: absolute; height: 250px; width: 15px; background-color: red;top: 0;left: ${((this.state.intensity - 1) / 9) * 250}px;`}></div>
+        <div
+          onMouseDown={this.handleMouseDown}
+          onMouseMove={this.handleMouseMove}
+          onMouseUp={this.handleMouseUp}
+          style={`position: absolute; height: 250px; width: 15px; background-color: red;top: 0;left: ${((this.state.intensity - 1) / 9) * 250}px;`}
+        />
       </div>
     )
   }
